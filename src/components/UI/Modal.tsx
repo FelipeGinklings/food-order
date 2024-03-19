b9803dfa1dbdfdc82e8d7ccaa9ea4721ab88e3d2
Felipe Ginklings <41814653+FelipeGinklings@users.noreply.github.com>
2024-03-18 23:45:48 -0300

    Making the modal more reusable

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 type Props = {
+  open: boolean;
   children: React.ReactNode;
   className?: string;
+  onClose: () => void;
 };
 
-const Modal: React.FC<Props> = ({ children, className }) => {
-  return (
+interface Dialog extends HTMLDialogElement {}
+
+const Modal: React.FC<Props> = ({ open, children, className, onClose }) => {
+  const dialog = useRef<Dialog>(null);
+
+  useEffect(() => {
+    if (open) {
+      dialog.current?.showModal();
+    } else {
+      dialog.current?.close();
+    }
+  }, [open]);
+
+  return createPortal(
     <dialog
       open
       className={`bg-orange-50 border-0 rounded-md shadow-lg p-4 w-4/5 max-w-[40rem] animate-fade-slide-up backdrop:bg-black-opacity-55 ${className}`}
+      ref={dialog}
+      onClose={onClose}
     >
       {children}
-    </dialog>
+    </dialog>,
+    document.getElementById('modals')!
   );
 };
 
